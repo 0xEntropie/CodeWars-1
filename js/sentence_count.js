b4a13af2d164483d25e3ac23f9e179d14d095d6e
hugmanrique <hugmanrique@gmail.com>
2017-03-10 17:21:39 -0500

    Add sentence count

---
+++ js/sentence_count.js
@@ -0,0 +1,10 @@
+/*
+Original Problem:
+Create a function that will tell how many sentences are in a paragraph, based on the number of periods ".", question marks "?" and "!" exclamation points that an input string contains.
+*/
+
+function sentenceCount(string) {
+  return (string.match(/\.|\?|!/g) || []).length;
+}
+
+console.log(sentenceCount("Hello. How are you? I'm pretty, fine."));
