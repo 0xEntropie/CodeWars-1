c1814657431822fea96a040afc3c7f0228b3bbfd
hugmanrique <hugmanrique@gmail.com>
2017-03-11 18:03:59 -0500

    Add test note

--- js/test.js
+++ js/test.js
@@ -1,3 +1,7 @@
+/*
+I use this file to transpile ES6 code to vanilla JS and run it with node test.js
+*/
+
 'use strict';
 
 function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
